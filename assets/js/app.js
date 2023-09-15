fc5e789977181422208bf90ddb87eb3eedbb413b
GopalAror <punjabijanu586@gmail.com>
2023-09-15 22:39:02 +0530

    responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,8 @@ let span6 = document.querySelector(".span6")
 
 menu.addEventListener("click",function(){
   mobileView.classList.toggle("show");
-  document.querySelector("body").classList.toggle("overflow-hidden")
-  span4.classList.toggle("span1")
-  span5.classList.toggle("span2")
-  span6.classList.toggle("span3")
+  document.querySelector("body").classList.toggle("overflow-hidden");
+  span4.classList.toggle("span1");
+  span5.classList.toggle("span2");
+  span6.classList.toggle("span3");
 })
